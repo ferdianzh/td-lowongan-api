2fed7fd39ef1ade4820b585af57da337f3a92e8f
ferdianzh <ferdianzh30@gmail.com>
2023-03-03 23:00:09 +0700

    update lowongan service

--- src/services/lowongan.service.js
+++ src/services/lowongan.service.js
@@ -1,16 +1,16 @@
 const db = require("../../db/models/index.js")
-const lowongan = db.lowongan
+const Lowongan = db.lowongan
 
 async function addLowongan(request) {
-  return await lowongan.create(request)
+  return await Lowongan.create(request)
 }
 
 async function getAllLowongan() {
-  return await lowongan.findAll()
+  return await Lowongan.findAll()
 }
 
 async function getLowonganById(id) {
-  const result = await lowongan.findByPk(id)
+  const result = await Lowongan.findByPk(id)
   if (!result) {
     throw Error(`lowongan with id=${id} not found`)
   }
@@ -18,19 +18,19 @@ async function getLowonganById(id) {
 }
 
 async function updateLowonganById(id, request) {
-  const result = await lowongan.update(request, { where: { id: id } })
+  const result = await Lowongan.update(request, { where: { id: id } })
   if (!result[0]) {
     throw Error(`lowongan with id=${id} not found`)
   }
-  return await lowongan.findByPk(id)
+  return await Lowongan.findByPk(id)
 }
 
 async function deleteLowonganById(id) {
-  const result = await lowongan.destroy({ where: { id: id } })
+  const result = await Lowongan.destroy({ where: { id: id } })
   if (!result) {
     throw Error(`lowongan with id=${id} not found`)
   }
-  return await lowongan.findByPk(id)
+  return await Lowongan.findByPk(id)
 }
 
 module.exports = {
